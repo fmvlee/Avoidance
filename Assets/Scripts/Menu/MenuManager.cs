8314d0e2d1e15d7e2d3e33b5b0ae6b0ed2a8fd5e
fmvlee <lee@javastudent.co.uk>
2023-07-13 21:18:00 +0100

    Prototype v3
    Implement example action recording to menu illustrating game play
    Created build v3

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -11,22 +11,51 @@ public class MenuManager : MonoBehaviour
     private GameObject hiScore;
     private GameObject globalHiScore;
 
+    private GameObject mainCanvas;
+    private GameObject recordingCanvas;
+
+    [SerializeField]
+    private bool isPlayingRecording;
+    [SerializeField]
+    private float recordingWaitTime;
+    [SerializeField]
+    private float recordingDefaultWaitTime = 3f;
     //private bool scoresUpdated = false;
 
     private void Awake()
     {
+        recordingWaitTime = recordingDefaultWaitTime;
+        isPlayingRecording = false;
         leaderboard = GetComponent<LeaderboardManager>();
         hiScore = GameObject.FindWithTag("HiScoreText");
         globalHiScore = GameObject.FindWithTag("GlobalHiScoreText");
+        mainCanvas = GameObject.FindWithTag("MainCanvas");
+        recordingCanvas = GameObject.FindWithTag("RecordingCanvas");
+        recordingCanvas.SetActive(false);
     }
 
     private void Update()
-    {
-        if (Keyboard.current.anyKey.wasPressedThisFrame)
+    {        
+        if (recordingWaitTime <= 0)
+        {
+            recordingCanvas.SetActive(true);
+            mainCanvas.SetActive(false);
+            isPlayingRecording = true;
+        } else
+        {
+            recordingWaitTime -= Time.deltaTime;
+        }
+
+        if (Keyboard.current.anyKey.wasPressedThisFrame && isPlayingRecording == false)
         {
             // Load the game
-            Debug.Log("Load game");
             SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        } else if (Keyboard.current.anyKey.wasPressedThisFrame && isPlayingRecording == true)
+        {
+            recordingCanvas.SetActive(false);
+            mainCanvas.SetActive(true);
+            recordingWaitTime = recordingDefaultWaitTime;
+            isPlayingRecording = false;
         }
 
         // Updates the score in the UI - Would be better to move to start metod
